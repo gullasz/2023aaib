6c6b52271544e992f7cbb1c29de1256e5d950e6a
gullasz <sun9403022@gmail.com>
2023-12-22 11:46:34 +0800

    15

---
+++ week15/week15-5b1.py
@@ -0,0 +1,16 @@
+a=input()
+a1,a2,a3,a4,a5,a6=0,0,0,0,0,0
+for c in a:
+	if c=='1':a1+=1
+	if c=='2':a2+=1
+	if c=='3':a3+=1
+	if c=='4':a4+=1
+	if c=='5':a5+=1
+	if c=='6':a6+=1
+print('1:',a1,sep='')
+print('2:',a2,sep='')
+print('3:',a3,sep='')
+print('4:',a4,sep='')
+print('5:',a5,sep='')
+print('6:',a6,sep='')
+
